cd87475e6423d10b4099b51a8f823381396893c8
Alok290 <79790013+Alok290@users.noreply.github.com>
2024-04-07 15:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function volume_sphere() {
     //Write your code here
+	cont form= document.getElementById(".MyForm");
+	let radius = Number(form.radius.value);
+	form.volume.value = (4*Math.PI*(r**3))/3
+
+	return flase;
   
 } 
 
