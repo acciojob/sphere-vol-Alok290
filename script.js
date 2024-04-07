1e8ad95245a5a8db428c32ec4592a51ab02032e3
Alok290 <79790013+Alok290@users.noreply.github.com>
2024-04-07 15:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function volume_sphere() {
     //Write your code here
-	cont form= document.getElementById(".MyForm");
+	const form= document.getElementById("MyForm");
 	let radius = Number(form.radius.value);
 	form.volume.value = (4*Math.PI*(r**3))/3
 
